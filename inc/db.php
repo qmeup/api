6d084160fa469be861fb86f2ad1fa0fde267923a
Alma Ohene-Opare <aopare@xublr.com>
2013-11-12 16:31:00 -0700

    Supress Hostname Error

--- inc/db.php
+++ inc/db.php
@@ -5,7 +5,7 @@ $password = "bc38bf1f";
 $hostname = "http://us-cdbr-azure-west-b.cleardb.com'";	
 $database = "qmeup";
 
-$con = mysql_connect($hostname, $username, $password) or die(mysql_error());
+$con = @mysql_connect($hostname, $username, $password) or die(mysql_error());
 mysql_select_db($database) or die(mysql_error());
 
 ?>
