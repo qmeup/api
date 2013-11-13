a07d94cd3ce5c8e3986ca33934b2cfc29c15136f
Alma Ohene-Opare <aopare@xublr.com>
2013-11-12 19:40:32 -0700

    Changed MySQL connection information

--- inc/db.php
+++ inc/db.php
@@ -1,10 +1,8 @@
 <?php
-$url=parse_url(getenv("mysql://bbfa6b7033a0a8:bc38bf1f@us-cdbr-azure-west-b.cleardb.com/qmeup?reconnect=true"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"],1);
+$server = "us-cdbr-azure-west-b.cleardb.com";
+$username = "bbfa6b7033a0a8";
+$password = "bc38bf1f";
+$db = "qmeup";
 
 mysql_connect($server, $username, $password);
 mysql_select_db($db) or die(mysql_error());
